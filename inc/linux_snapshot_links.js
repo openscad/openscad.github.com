785b3aa1257f67a6defb5be4fa8521b3fbe7f391
Don Bright <hugh.m.bright@gmail.com>
2013-05-14 21:20:21 -0500

    allow size of file to be specified

--- inc/linux_snapshot_links.js
+++ inc/linux_snapshot_links.js
@@ -2,4 +2,6 @@ snapshot_links['LIN32_SNAPSHOT_URL'] = 'https://openscad.googlecode.com/files/op
 snapshot_links['LIN64_SNAPSHOT_URL'] = 'https://openscad.googlecode.com/files/openscad-2013.01.x86-64.tar.gz';
 snapshot_names['LIN32_SNAPSHOT_NAME'] = 'OpenSCAD 2013.01';
 snapshot_names['LIN64_SNAPSHOT_NAME'] = 'OpenSCAD 2013.01';
+snapshot_sizes['LIN32_SNAPSHOT_SIZE'] = '15 MB';
+snapshot_sizes['LIN64_SNAPSHOT_SIZE'] = '17 MB';
 
--- inc/win_snapshot_links.js
+++ inc/win_snapshot_links.js
@@ -7,3 +7,9 @@ snapshot_names['WIN32_SNAPSHOT1_NAME'] = 'OpenSCAD 2013.05.12';
 snapshot_names['WIN32_SNAPSHOT2_NAME'] = 'OpenSCAD 2013.05.12';
 snapshot_names['WIN64_SNAPSHOT1_NAME'] = 'OpenSCAD 2013.05.12';
 snapshot_names['WIN64_SNAPSHOT2_NAME'] = 'OpenSCAD 2013.05.12';
+
+snapshot_sizes['WIN32_SNAPSHOT1_SIZE'] = '7.5 MB';
+snapshot_sizes['WIN32_SNAPSHOT2_SIZE'] = '7.5 MB';
+snapshot_sizes['WIN64_SNAPSHOT1_SIZE'] = '7.5 MB';
+snapshot_sizes['WIN64_SNAPSHOT2_SIZE'] = '7.5 MB';
+
