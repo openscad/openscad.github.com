a0a84efeee9252f82b1c8802d5d8303f7512e12b
Don Bright <hugh.m.bright@gmail.com>
2013-05-15 19:17:55 -0500

    builder.sh - updated snapshot links

--- inc/win_snapshot_links.js
+++ inc/win_snapshot_links.js
@@ -1,15 +1,6 @@
-snapinfo['WIN32_SNAPSHOT1_URL'] = 'https://openscad.googlecode.com/files/OpenSCAD-2013.05.12-x86-32-Installer.exe';
-snapinfo['WIN32_SNAPSHOT2_URL'] = 'https://openscad.googlecode.com/files/OpenSCAD-2013.05.12-x86-32.zip';
-snapinfo['WIN64_SNAPSHOT1_URL'] = 'https://openscad.googlecode.com/files/OpenSCAD-2013.05.12-x86-64.zip';
-snapinfo['WIN64_SNAPSHOT2_URL'] = 'https://openscad.googlecode.com/files/OpenSCAD-2013.05.12-x86-64.zip';
-
-snapinfo['WIN32_SNAPSHOT1_NAME'] = 'OpenSCAD 2013.05.12';
-snapinfo['WIN32_SNAPSHOT2_NAME'] = 'OpenSCAD 2013.05.12';
-snapinfo['WIN64_SNAPSHOT1_NAME'] = 'OpenSCAD 2013.05.12';
-snapinfo['WIN64_SNAPSHOT2_NAME'] = 'OpenSCAD 2013.05.12';
-
-snapinfo['WIN32_SNAPSHOT1_SIZE'] = '7.5 MB';
-snapinfo['WIN32_SNAPSHOT2_SIZE'] = '7.5 MB';
-snapinfo['WIN64_SNAPSHOT1_SIZE'] = '7.5 MB';
-snapinfo['WIN64_SNAPSHOT2_SIZE'] = '7.5 MB';
-
+snapinfo['WIN32_SNAPSHOT1_URL'] = 'https://openscad.google.com/files/OpenSCAD-2013.05.15-x86-32.zip'
+snapinfo['WIN32_SNAPSHOT2_URL'] = 'https://openscad.google.com/files/OpenSCAD-2013.05.15-x86-32-Installer.exe'
+snapinfo['WIN32_SNAPSHOT1_NAME'] = 'OpenSCAD 2013.05.15'
+snapinfo['WIN32_SNAPSHOT2_NAME'] = 'OpenSCAD 2013.05.15'
+snapinfo['WIN32_SNAPSHOT1_SIZE'] = '7.6M'
+snapinfo['WIN32_SNAPSHOT2_SIZE'] = '7.6M'
