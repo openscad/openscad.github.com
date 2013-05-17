bec3f10c710ae75ec06073e39e36b222feda2536
Don Bright <hugh.m.bright@gmail.com>
2013-05-17 17:55:08 -0500

    quick fix bad url

--- inc/win_snapshot_links.js
+++ inc/win_snapshot_links.js
@@ -1,5 +1,5 @@
-snapinfo['WIN32_SNAPSHOT1_URL'] = 'https://openscad.google.com/files/OpenSCAD-2013.05.15-x86-32.zip'
-snapinfo['WIN32_SNAPSHOT2_URL'] = 'https://openscad.google.com/files/OpenSCAD-2013.05.15-x86-32-Installer.exe'
+snapinfo['WIN32_SNAPSHOT1_URL'] = 'https://openscad.googlecode.com/files/OpenSCAD-2013.05.15-x86-32.zip'
+snapinfo['WIN32_SNAPSHOT2_URL'] = 'https://openscad.googlecode.com/files/OpenSCAD-2013.05.15-x86-32-Installer.exe'
 snapinfo['WIN32_SNAPSHOT1_NAME'] = 'OpenSCAD 2013.05.15'
 snapinfo['WIN32_SNAPSHOT2_NAME'] = 'OpenSCAD 2013.05.15'
 snapinfo['WIN32_SNAPSHOT1_SIZE'] = '7.6MB'
