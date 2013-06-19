2ec840dcf054a51556409d06b66307f7df2235d2
Marius Kintel <marius@kintel.net>
2013-06-18 22:33:06 -0400

    Updated download links to 2013.06

--- inc/linux_snapshot_links.js
+++ inc/linux_snapshot_links.js
@@ -1,8 +1,7 @@
-fileinfo['LIN32_SNAPSHOT_URL'] = 'https://openscad.googlecode.com/files/openscad-2013.01.x86-32.tar.gz';
-fileinfo['LIN32_SNAPSHOT_NAME'] = 'OpenSCAD 2013.01';
-fileinfo['LIN32_SNAPSHOT_SIZE'] = '15 MB';
+fileinfo['LIN32_SNAPSHOT_URL'] = 'https://openscad.googlecode.com/files/openscad-2013.06.x86-32.tar.gz';
+fileinfo['LIN32_SNAPSHOT_NAME'] = 'OpenSCAD 2013.06';
+fileinfo['LIN32_SNAPSHOT_SIZE'] = '17 MB';
 
-fileinfo['LIN64_SNAPSHOT_URL'] = 'https://openscad.googlecode.com/files/openscad-2013.01.x86-64.tar.gz';
-fileinfo['LIN64_SNAPSHOT_NAME'] = 'OpenSCAD 2013.01';
+fileinfo['LIN64_SNAPSHOT_URL'] = 'https://openscad.googlecode.com/files/openscad-2013.06.x86-64.tar.gz';
+fileinfo['LIN64_SNAPSHOT_NAME'] = 'OpenSCAD 2013.06';
 fileinfo['LIN64_SNAPSHOT_SIZE'] = '17 MB';
-
--- inc/mac_release_links.js
+++ inc/mac_release_links.js
@@ -1,3 +1,3 @@
-fileinfo['MAC_RELEASE_URL'] = 'https://openscad.googlecode.com/files/OpenSCAD-2013.01.dmg'
-fileinfo['MAC_RELEASE_NAME'] = 'OpenSCAD 2013.01'
-fileinfo['MAC_RELEASE_SIZE'] = '22 MB'
+fileinfo['MAC_RELEASE_URL'] = 'https://openscad.googlecode.com/files/OpenSCAD-2013.06.dmg'
+fileinfo['MAC_RELEASE_NAME'] = 'OpenSCAD 2013.06'
+fileinfo['MAC_RELEASE_SIZE'] = '23.3 MB'
