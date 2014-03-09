aa86fa778afdbf996436d343e6022d84bdd5097e
Torsten Paul <Torsten.Paul@gmx.de>
2014-03-09 14:27:15 +0100

    Update links for 2014.03 release.

--- inc/linux_release_links.js
+++ inc/linux_release_links.js
@@ -1,7 +1,7 @@
-fileinfo['LIN32_RELEASE_URL'] = 'https://openscad.googlecode.com/files/openscad-2013.06.x86-32.tar.gz';
-fileinfo['LIN32_RELEASE_NAME'] = 'OpenSCAD 2013.06';
-fileinfo['LIN32_RELEASE_SIZE'] = '17 MB';
+fileinfo['LIN32_RELEASE_URL'] = 'http://files.openscad.org/openscad-2014.03.x86-32.tar.gz';
+fileinfo['LIN32_RELEASE_NAME'] = 'OpenSCAD 2014.03';
+fileinfo['LIN32_RELEASE_SIZE'] = '22 MB';
 
-fileinfo['LIN64_RELEASE_URL'] = 'https://openscad.googlecode.com/files/openscad-2013.06.x86-64.tar.gz';
-fileinfo['LIN64_RELEASE_NAME'] = 'OpenSCAD 2013.06';
-fileinfo['LIN64_RELEASE_SIZE'] = '17 MB';
+fileinfo['LIN64_RELEASE_URL'] = 'http://files.openscad.org/openscad-2014.03.x86-64.tar.gz';
+fileinfo['LIN64_RELEASE_NAME'] = 'OpenSCAD 2014.03';
+fileinfo['LIN64_RELEASE_SIZE'] = '22 MB';
