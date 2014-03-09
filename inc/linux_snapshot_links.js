eb627293dc31ec7155132e5777b547f25c0c073f
Marius Kintel <marius@kintel.net>
2014-03-09 18:08:20 -0400

    Updated linux and windows snapshots to point to latest release

--- inc/linux_snapshot_links.js
+++ inc/linux_snapshot_links.js
@@ -2,6 +2,6 @@ fileinfo['LIN32_SNAPSHOT_URL'] = 'http://files.openscad.org/openscad-2014.03.x86
 fileinfo['LIN32_SNAPSHOT_NAME'] = 'OpenSCAD 2014.03';
 fileinfo['LIN32_SNAPSHOT_SIZE'] = '21.9 MB';
 
-fileinfo['LIN64_SNAPSHOT_URL'] = 'http://files.openscad.org/openscad-2014.03.04.x86-64.tar.gz';
-fileinfo['LIN64_SNAPSHOT_NAME'] = 'OpenSCAD 2014.03.04';
+fileinfo['LIN64_SNAPSHOT_URL'] = 'http://files.openscad.org/openscad-2014.03.x86-64.tar.gz';
+fileinfo['LIN64_SNAPSHOT_NAME'] = 'OpenSCAD 2014.03';
 fileinfo['LIN64_SNAPSHOT_SIZE'] = '19.9 MB';
