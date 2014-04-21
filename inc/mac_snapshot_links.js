67208a09dfa9c5816b17936bea583cf5e5c9ead0
Marius Kintel <marius@kintel.net>
2014-04-21 12:34:16 -0400

    Rebuilt binary to work on Mac OS X 10.7

--- inc/mac_snapshot_links.js
+++ inc/mac_snapshot_links.js
@@ -1,3 +1,3 @@
 fileinfo['MAC_SNAPSHOT_URL'] = 'http://files.openscad.org/OpenSCAD-2014.03.dmg'
 fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2014.03'
-fileinfo['MAC_SNAPSHOT_SIZE'] = '11.5 MB'
+fileinfo['MAC_SNAPSHOT_SIZE'] = '13.4 MB'
