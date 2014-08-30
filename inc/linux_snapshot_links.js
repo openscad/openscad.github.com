26f135f4ceaef64a5e83c6a7d64e053f6814f212
Torsten Paul <Torsten.Paul@gmx.de>
2014-08-31 01:46:23 +0200

    Update Linux snapshot links.

--- inc/linux_snapshot_links.js
+++ inc/linux_snapshot_links.js
@@ -1,7 +1,7 @@
-fileinfo['LIN32_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/openscad-2014.05.31.x86-32.tar.gz';
-fileinfo['LIN32_SNAPSHOT_NAME'] = 'OpenSCAD 2014.05.31';
-fileinfo['LIN32_SNAPSHOT_SIZE'] = '26.3 MB';
+fileinfo['LIN32_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/openscad-2014.08.30.x86-32.tar.gz';
+fileinfo['LIN32_SNAPSHOT_NAME'] = 'OpenSCAD 2014.08.30';
+fileinfo['LIN32_SNAPSHOT_SIZE'] = '35.6 MB';
 
-fileinfo['LIN64_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/openscad-2014.05.31.x86-64.tar.gz';
-fileinfo['LIN64_SNAPSHOT_NAME'] = 'OpenSCAD 2014.05.31';
-fileinfo['LIN64_SNAPSHOT_SIZE'] = '26.2 MB';
+fileinfo['LIN64_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/openscad-2014.08.30.x86-64.tar.gz';
+fileinfo['LIN64_SNAPSHOT_NAME'] = 'OpenSCAD 2014.08.30';
+fileinfo['LIN64_SNAPSHOT_SIZE'] = '34.8 MB';
