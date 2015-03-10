2e28ebbeb63ae2134fa73346f75a6ffc68966f03
Marius Kintel <marius@kintel.net>
2015-03-10 16:25:41 -0400

    OpenSCAD-2015.03 released

--- inc/mac_snapshot_links.js
+++ inc/mac_snapshot_links.js
@@ -1,3 +1,3 @@
-fileinfo['MAC_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/OpenSCAD-2015.03.04.dmg'
-fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2015.03.04'
-fileinfo['MAC_SNAPSHOT_SIZE'] = '21.4 MB'
+fileinfo['MAC_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/OpenSCAD-2015.03.dmg'
+fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2015.03'
+fileinfo['MAC_SNAPSHOT_SIZE'] = '21.3 MB'
--- inc/src_release_links.js
+++ inc/src_release_links.js
@@ -1,3 +1,3 @@
-fileinfo['SRC_RELEASE_URL'] = 'http://files.openscad.org/openscad-2014.03.src.tar.gz'
-fileinfo['SRC_RELEASE_NAME'] = 'OpenSCAD 2014.03'
-fileinfo['SRC_RELEASE_SIZE'] = '4.2 MB'
+fileinfo['SRC_RELEASE_URL'] = 'http://files.openscad.org/openscad-2015.03.src.tar.gz'
+fileinfo['SRC_RELEASE_NAME'] = 'OpenSCAD 2015.03'
+fileinfo['SRC_RELEASE_SIZE'] = '13 MB'
