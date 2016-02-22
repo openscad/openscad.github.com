900db9529bb4d37843974a4456bcad76f37e18e0
Marius Kintel <marius@kintel.net>
2016-02-22 16:50:37 -0500

    Updated Mac binaries

--- inc/mac_release_links.js
+++ inc/mac_release_links.js
@@ -1,3 +1,3 @@
-fileinfo['MAC_RELEASE_URL'] = 'http://files.openscad.org/OpenSCAD-2015.03-2.dmg'
-fileinfo['MAC_RELEASE_NAME'] = 'OpenSCAD 2015.03-2'
-fileinfo['MAC_RELEASE_SIZE'] = '22.9 MB'
+fileinfo['MAC_RELEASE_URL'] = 'http://files.openscad.org/OpenSCAD-2015.03-3.dmg'
+fileinfo['MAC_RELEASE_NAME'] = 'OpenSCAD 2015.03-3'
+fileinfo['MAC_RELEASE_SIZE'] = '23 MB'
