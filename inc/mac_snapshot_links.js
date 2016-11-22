9f725e0fd591467724941f55ad7a7d5cc3a5e8d7
Marius Kintel <marius@kintel.net>
2016-11-21 21:34:58 -0500

    Updated Mac snapshot

--- inc/mac_snapshot_links.js
+++ inc/mac_snapshot_links.js
@@ -1,3 +1,3 @@
-fileinfo['MAC_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/OpenSCAD-2016.11.06.dmg'
-fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2016.11.06'
-fileinfo['MAC_SNAPSHOT_SIZE'] = '24.1 MB'
+fileinfo['MAC_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/OpenSCAD-2016.11.21.dmg'
+fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2016.11.21'
+fileinfo['MAC_SNAPSHOT_SIZE'] = '22.3 MB'
