dcdb8b0077b747cbeb46ff8d376587778c864746
Marius Kintel <marius@kintel.net>
2018-11-05 22:58:14 -0500

    Updated Mac binaries

--- inc/mac_snapshot_links.js
+++ inc/mac_snapshot_links.js
@@ -1,3 +1,3 @@
-fileinfo['MAC_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/OpenSCAD-2018.03.17.dmg'
-fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2018.03.17'
-fileinfo['MAC_SNAPSHOT_SIZE'] = '26.0 MB'
+fileinfo['MAC_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/OpenSCAD-2018.11.05.dmg'
+fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2018.11.05'
+fileinfo['MAC_SNAPSHOT_SIZE'] = '28.5 MB'
