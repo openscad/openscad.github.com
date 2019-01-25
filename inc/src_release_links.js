9b450e0ade0caade2f9991ba09574415600148b8
Torsten Paul <Torsten.Paul@gmx.de>
2019-01-25 16:53:11 +0100

    Update link to HTTP and use 2015.03-3.

--- inc/src_release_links.js
+++ inc/src_release_links.js
@@ -1,3 +1,3 @@
-fileinfo['SRC_RELEASE_URL'] = 'http://files.openscad.org/openscad-2015.03-2.src.tar.gz'
-fileinfo['SRC_RELEASE_NAME'] = 'OpenSCAD 2015.03-2'
+fileinfo['SRC_RELEASE_URL'] = 'https://files.openscad.org/openscad-2015.03-3.src.tar.gz'
+fileinfo['SRC_RELEASE_NAME'] = 'OpenSCAD 2015.03-3'
 fileinfo['SRC_RELEASE_SIZE'] = '13 MB'
