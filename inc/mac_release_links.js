78935fdb999bde22646ec25e517bccb85c11edfb
Torsten Paul <Torsten.Paul@gmx.de>
2019-01-25 16:50:30 +0100

    Update link to HTTPS

--- inc/mac_release_links.js
+++ inc/mac_release_links.js
@@ -1,3 +1,3 @@
-fileinfo['MAC_RELEASE_URL'] = 'http://files.openscad.org/OpenSCAD-2015.03-3.dmg'
+fileinfo['MAC_RELEASE_URL'] = 'https://files.openscad.org/OpenSCAD-2015.03-3.dmg'
 fileinfo['MAC_RELEASE_NAME'] = 'OpenSCAD 2015.03-3'
 fileinfo['MAC_RELEASE_SIZE'] = '23 MB'
