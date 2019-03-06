fa48a4def1e6cb402d377b80ce70f9d6e7c60ac3
Marius Kintel <marius@kintel.net>
2019-03-05 22:42:04 -0500

    Updated Mac binaries

--- inc/mac_snapshot_links.js
+++ inc/mac_snapshot_links.js
@@ -1,3 +1,3 @@
-fileinfo['MAC_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/OpenSCAD-2019.02.23.dmg'
-fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2019.02.23'
+fileinfo['MAC_SNAPSHOT_URL'] = 'http://files.openscad.org/snapshots/OpenSCAD-2019.03.03.dmg'
+fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2019.03.03'
 fileinfo['MAC_SNAPSHOT_SIZE'] = '24.7 MB'
