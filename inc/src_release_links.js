770d30faf8a0e4979a5795492baba550acce6d3e
Marius Kintel <marius@kintel.net>
2019-05-19 15:27:15 -0400

    Added pgp signature for src release

--- inc/src_release_links.js
+++ inc/src_release_links.js
@@ -1,3 +1,4 @@
 fileinfo['SRC_RELEASE_URL'] = 'https://files.openscad.org/openscad-2019.05.src.tar.gz'
 fileinfo['SRC_RELEASE_NAME'] = 'OpenSCAD 2019.05'
 fileinfo['SRC_RELEASE_SIZE'] = '14 MB'
+fileinfo['SRC_RELEASE_ASC_URL'] = fileinfo['SRC_RELEASE_URL'] + '.asc'
