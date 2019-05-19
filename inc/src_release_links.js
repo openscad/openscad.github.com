ec02525ac12a2f236686000c7768219e043f49d8
Marius Kintel <marius@kintel.net>
2019-05-19 10:36:24 -0400

    Added source archive

--- inc/src_release_links.js
+++ inc/src_release_links.js
@@ -1,3 +1,3 @@
-fileinfo['SRC_RELEASE_URL'] = 'https://files.openscad.org/openscad-2015.03-3.src.tar.gz'
-fileinfo['SRC_RELEASE_NAME'] = 'OpenSCAD 2015.03-3'
-fileinfo['SRC_RELEASE_SIZE'] = '13 MB'
+fileinfo['SRC_RELEASE_URL'] = 'https://files.openscad.org/openscad-2019.05.src.tar.gz'
+fileinfo['SRC_RELEASE_NAME'] = 'OpenSCAD 2019.05'
+fileinfo['SRC_RELEASE_SIZE'] = '14 MB'
