c33c53a3e10f3d6e0054c20d50e1a59e6960674c
Marius Kintel <marius@kintel.net>
2019-05-19 15:39:05 -0400

    Removed sha1 signatures to make space for pgp

--- inc/linux_snapshot_links.js
+++ inc/linux_snapshot_links.js
@@ -5,6 +5,5 @@ fileinfo['LIN64_SNAPSHOT_URL'] = 'https://files.openscad.org/snapshots/OpenSCAD-
 fileinfo['LIN64_SNAPSHOT_NAME'] = fileinfo['LIN_SNAPSHOT_NAME']
 fileinfo['LIN64_SNAPSHOT_SIZE'] = fileinfo['LIN_SNAPSHOT_SIZE']
 fileinfo['LIN64_SNAPSHOT_ASC_URL'] = fileinfo['LIN64_SNAPSHOT_URL'] + '.asc'
-fileinfo['LIN64_SNAPSHOT_SHA1_URL'] = fileinfo['LIN64_SNAPSHOT_URL'] + '.sha1'
 fileinfo['LIN64_SNAPSHOT_SHA256_URL'] = fileinfo['LIN64_SNAPSHOT_URL'] + '.sha256'
 fileinfo['LIN64_SNAPSHOT_SHA512_URL'] = fileinfo['LIN64_SNAPSHOT_URL'] + '.sha512'
