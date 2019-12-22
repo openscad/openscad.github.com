0ae50039140ecdc24a5a6a531b8b7a4441fd8d4e
Torsten Paul <Torsten.Paul@gmx.de>
2019-12-22 03:42:31 +0100

    Update snapshots.

--- inc/linux_snapshot_links.js
+++ inc/linux_snapshot_links.js
@@ -1,6 +1,6 @@
-fileinfo['LIN_SNAPSHOT_NAME']   = 'OpenSCAD-2019.11.20'
+fileinfo['LIN_SNAPSHOT_NAME']   = 'OpenSCAD-2019.12.21'
 fileinfo['LIN_SNAPSHOT_SIZE']   = '35 MB'
-fileinfo['LIN64_SNAPSHOT_URL']  = 'https://files.openscad.org/snapshots/OpenSCAD-2019.11.20.ai3988-eedfcc4-x86_64.AppImage'
+fileinfo['LIN64_SNAPSHOT_URL']  = 'https://files.openscad.org/snapshots/OpenSCAD-2019.12.21.ai4163-4ef45b0-x86_64.AppImage'
 
 fileinfo['LIN64_SNAPSHOT_NAME'] = fileinfo['LIN_SNAPSHOT_NAME']
 fileinfo['LIN64_SNAPSHOT_SIZE'] = fileinfo['LIN_SNAPSHOT_SIZE']
