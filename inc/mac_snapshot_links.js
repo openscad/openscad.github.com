73a41cbb05cb5aee363c5e910108853ed84d07f1
Torsten Paul <Torsten.Paul@gmx.de>
2020-04-25 00:37:13 +0200

    Update snapshots.

--- inc/mac_snapshot_links.js
+++ inc/mac_snapshot_links.js
@@ -1,5 +1,5 @@
-fileinfo['MAC_SNAPSHOT_URL'] = 'https://files.openscad.org/snapshots/OpenSCAD-2019.12.20.dmg'
-fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2019.12.20'
+fileinfo['MAC_SNAPSHOT_URL'] = 'https://files.openscad.org/snapshots/OpenSCAD-2020.04.23.dmg'
+fileinfo['MAC_SNAPSHOT_NAME'] = 'OpenSCAD 2020.04.23'
 fileinfo['MAC_SNAPSHOT_SIZE'] = '25 MB'
 fileinfo['MAC_SNAPSHOT_SHA256_URL'] = fileinfo['MAC_SNAPSHOT_URL'] + '.sha256'
 fileinfo['MAC_SNAPSHOT_SHA512_URL'] = fileinfo['MAC_SNAPSHOT_URL'] + '.sha512'
