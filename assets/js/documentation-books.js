808c4c89e7e0ccab1bf5061a00badfb82d0d566d
Torsten Paul <Torsten.Paul@gmx.de>
2020-11-29 02:15:00 +0100

    Add book title image for "Creating 3D Models for 3D Printing Using OpenSCAD".

---
+++ assets/js/documentation-books.js
@@ -0,0 +1,20 @@
+/* <![CDATA[ */
+    (function() {
+        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'https://api.flattr.com/js/0.6/load.js?mode=auto&button=compact';
+        t.parentNode.insertBefore(s, t);
+    })();
+/* ]]> */
+
+var pageHeight = $(window).height();
+$(document).ready(function() {
+	
+	$('.top').click(function(){$('body').scrollTo($('body'),700,{offset:{top:-20}});});
+	
+	$('.sticky').waypoint('sticky');
+	
+	$('#sidebar.sticky').parent().css('width',$("#sidebar.sticky").css('width'));
+
+});
