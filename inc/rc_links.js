1f4272f7d14d40faf1412ec44468a5ca0e86605f
Torsten Paul <Torsten.Paul@gmx.de>
2020-12-18 22:50:59 +0100

    Release candidate 2020.12-RC3.

--- inc/rc_links.js
+++ inc/rc_links.js
@@ -1,62 +1,61 @@
-fileinfo['MAC_RC_URL'] = 'N/A'
-fileinfo['MAC_RC_NAME'] = 'OpenSCAD 2020.12-RC2'
-fileinfo['MAC_RC_SIZE'] = '?? MB'
+fileinfo['MAC_RC_URL']    = 'N/A'
+fileinfo['MAC_RC_NAME']   = 'OpenSCAD 2020.12-RC3'
+fileinfo['MAC_RC_SIZE']   = '?? MB'
 
-fileinfo['WIN_RC_NAME']   = 'OpenSCAD 2020.12-RC2'
-fileinfo['WIN_RC_SIZE']   = '20 MB'
-fileinfo['WIN32_RC_BASE'] = 'https://files.openscad.org/OpenSCAD-2020.12-RC2-x86-32'
-fileinfo['WIN64_RC_BASE'] = 'https://files.openscad.org/OpenSCAD-2020.12-RC2-x86-64'
+fileinfo['WIN_RC_NAME']   = 'OpenSCAD 2020.12-RC3'
+fileinfo['WIN32_RC_SIZE'] = '20 MB'
+fileinfo['WIN32_RC_BASE'] = 'https://files.openscad.org/rc/OpenSCAD-2020.12-RC3-x86-32'
+fileinfo['WIN64_RC_SIZE'] = '21 MB'
+fileinfo['WIN64_RC_BASE'] = 'https://files.openscad.org/rc/OpenSCAD-2020.12-RC3-x86-64'
 
-fileinfo['LIN_RC_NAME']   = 'OpenSCAD 2020.12-RC2'
-fileinfo['LIN_RC_SIZE']   = '39 MB'
+fileinfo['LIN_RC_NAME']   = 'OpenSCAD 2020.12-RC3'
+fileinfo['LIN32_RC_SIZE'] = 'N/A'
+fileinfo['LIN32_RC_URL']  = 'N/A'
+fileinfo['LIN64_RC_SIZE'] = '39 MB'
+fileinfo['LIN64_RC_URL']  = 'https://files.openscad.org/rc/OpenSCAD-2020.12-RC3-x86_64.AppImage'
 
-fileinfo['LIN32_RC_URL'] = 'N/A'
-fileinfo['LIN64_RC_URL'] = 'https://files.openscad.org/OpenSCAD-2020.12-RC2-x86_64.AppImage'
-
-fileinfo['SRC_RC_URL'] = 'https://files.openscad.org/openscad-2020.12-RC2.src.tar.gz'
-fileinfo['SRC_RC_NAME'] = 'OpenSCAD 2020.12-RC2'
-fileinfo['SRC_RC_SIZE'] = '16 MB'
+fileinfo['SRC_RC_URL']    = 'https://files.openscad.org/rc/openscad-2020.12-RC3.src.tar.gz'
+fileinfo['SRC_RC_NAME']   = 'OpenSCAD 2020.12-RC3'
+fileinfo['SRC_RC_SIZE']   = '16 MB'
 
 fileinfo['MAC_RC_SHA256_URL'] = fileinfo['MAC_RC_URL'] + '.sha256'
 fileinfo['MAC_RC_SHA512_URL'] = fileinfo['MAC_RC_URL'] + '.sha512'
 
 fileinfo['WIN32_RC_INSTALLER_URL'] = fileinfo['WIN32_RC_BASE'] + '-Installer.exe'
 fileinfo['WIN32_RC_INSTALLER_NAME'] = fileinfo['WIN_RC_NAME']
-fileinfo['WIN32_RC_INSTALLER_SIZE'] = fileinfo['WIN_RC_SIZE']
+fileinfo['WIN32_RC_INSTALLER_SIZE'] = fileinfo['WIN32_RC_SIZE']
 fileinfo['WIN32_RC_INSTALLER_ASC_URL'] = fileinfo['WIN32_RC_INSTALLER_URL'] + '.asc'
 fileinfo['WIN32_RC_INSTALLER_SHA1_URL'] = fileinfo['WIN32_RC_INSTALLER_URL'] + '.sha1'
 fileinfo['WIN32_RC_INSTALLER_SHA256_URL'] = fileinfo['WIN32_RC_INSTALLER_URL'] + '.sha256'
 fileinfo['WIN32_RC_INSTALLER_SHA512_URL'] = fileinfo['WIN32_RC_INSTALLER_URL'] + '.sha512'
 fileinfo['WIN32_RC_ZIP_URL'] = fileinfo['WIN32_RC_BASE'] + '.zip'
 fileinfo['WIN32_RC_ZIP_NAME'] = fileinfo['WIN_RC_NAME']
-fileinfo['WIN32_RC_ZIP_SIZE'] = fileinfo['WIN_RC_SIZE']
+fileinfo['WIN32_RC_ZIP_SIZE'] = fileinfo['WIN32_RC_SIZE']
 fileinfo['WIN32_RC_ZIP_ASC_URL'] = fileinfo['WIN32_RC_ZIP_URL'] + '.asc'
 fileinfo['WIN32_RC_ZIP_SHA1_URL'] = fileinfo['WIN32_RC_ZIP_URL'] + '.sha1'
 fileinfo['WIN32_RC_ZIP_SHA256_URL'] = fileinfo['WIN32_RC_ZIP_URL'] + '.sha256'
 fileinfo['WIN32_RC_ZIP_SHA512_URL'] = fileinfo['WIN32_RC_ZIP_URL'] + '.sha512'
 fileinfo['WIN64_RC_INSTALLER_URL'] = fileinfo['WIN64_RC_BASE'] + '-Installer.exe'
 fileinfo['WIN64_RC_INSTALLER_NAME'] = fileinfo['WIN_RC_NAME']
-fileinfo['WIN64_RC_INSTALLER_SIZE'] = fileinfo['WIN_RC_SIZE']
+fileinfo['WIN64_RC_INSTALLER_SIZE'] = fileinfo['WIN64_RC_SIZE']
 fileinfo['WIN64_RC_INSTALLER_ASC_URL'] = fileinfo['WIN64_RC_INSTALLER_URL'] + '.asc'
 fileinfo['WIN64_RC_INSTALLER_SHA1_URL'] = fileinfo['WIN64_RC_INSTALLER_URL'] + '.sha1'
 fileinfo['WIN64_RC_INSTALLER_SHA256_URL'] = fileinfo['WIN64_RC_INSTALLER_URL'] + '.sha256'
 fileinfo['WIN64_RC_INSTALLER_SHA512_URL'] = fileinfo['WIN64_RC_INSTALLER_URL'] + '.sha512'
 fileinfo['WIN64_RC_ZIP_URL'] = fileinfo['WIN64_RC_BASE'] + '.zip'
 fileinfo['WIN64_RC_ZIP_NAME'] = fileinfo['WIN_RC_NAME']
-fileinfo['WIN64_RC_ZIP_SIZE'] = fileinfo['WIN_RC_SIZE']
+fileinfo['WIN64_RC_ZIP_SIZE'] = fileinfo['WIN64_RC_SIZE']
 fileinfo['WIN64_RC_ZIP_ASC_URL'] = fileinfo['WIN64_RC_ZIP_URL'] + '.asc'
 fileinfo['WIN64_RC_ZIP_SHA1_URL'] = fileinfo['WIN64_RC_ZIP_URL'] + '.sha1'
 fileinfo['WIN64_RC_ZIP_SHA256_URL'] = fileinfo['WIN64_RC_ZIP_URL'] + '.sha256'
 fileinfo['WIN64_RC_ZIP_SHA512_URL'] = fileinfo['WIN64_RC_ZIP_URL'] + '.sha512'
 
 fileinfo['LIN32_RC_NAME'] = fileinfo['LIN_RC_NAME']
-fileinfo['LIN32_RC_SIZE'] = fileinfo['LIN_RC_SIZE']
 fileinfo['LIN32_RC_ASC_URL'] = fileinfo['LIN32_RC_URL'] + '.asc'
 fileinfo['LIN32_RC_SHA256_URL'] = fileinfo['LIN32_RC_URL'] + '.sha256'
 fileinfo['LIN32_RC_SHA512_URL'] = fileinfo['LIN32_RC_URL'] + '.sha512'
 
 fileinfo['LIN64_RC_NAME'] = fileinfo['LIN_RC_NAME']
-fileinfo['LIN64_RC_SIZE'] = fileinfo['LIN_RC_SIZE']
 fileinfo['LIN64_RC_ASC_URL'] = fileinfo['LIN64_RC_URL'] + '.asc'
 fileinfo['LIN64_RC_SHA256_URL'] = fileinfo['LIN64_RC_URL'] + '.sha256'
 fileinfo['LIN64_RC_SHA512_URL'] = fileinfo['LIN64_RC_URL'] + '.sha512'
