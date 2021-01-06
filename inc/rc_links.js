ebaa2b856ad73ee8d95f6f1a45eb83eb9fa28d55
Torsten Paul <Torsten.Paul@gmx.de>
2021-01-06 07:18:09 +0100

    Release candidate 2021.01-RC5.

--- inc/rc_links.js
+++ inc/rc_links.js
@@ -1,26 +1,27 @@
-fileinfo['VERSION']       = 'OpenSCAD 2020.12-RC4'
+fileinfo['VERSION']       = 'OpenSCAD 2021.01-RC5'
 
-fileinfo['MAC_RC_URL']    = 'N/A'
-fileinfo['MAC_RC_SIZE']   = '?? MB'
+fileinfo['MAC_RC_URL']    = 'https://files.openscad.org/rc/OpenSCAD-2021.01-RC5.dmg'
+fileinfo['MAC_RC_SIZE']   = '27 MB'
 
 fileinfo['WIN32_RC_SIZE'] = '20 MB'
-fileinfo['WIN32_RC_BASE'] = 'https://files.openscad.org/rc/OpenSCAD-2020.12-RC4-x86-32'
+fileinfo['WIN32_RC_BASE'] = 'https://files.openscad.org/rc/OpenSCAD-2021.01-RC5-x86-32'
 fileinfo['WIN64_RC_SIZE'] = '21 MB'
-fileinfo['WIN64_RC_BASE'] = 'https://files.openscad.org/rc/OpenSCAD-2020.12-RC4-x86-64'
+fileinfo['WIN64_RC_BASE'] = 'https://files.openscad.org/rc/OpenSCAD-2021.01-RC5-x86-64'
 
 fileinfo['LIN32_RC_SIZE'] = 'N/A'
 fileinfo['LIN32_RC_URL']  = 'N/A'
 fileinfo['LIN64_RC_SIZE'] = '39 MB'
-fileinfo['LIN64_RC_URL']  = 'https://files.openscad.org/rc/OpenSCAD-2020.12-RC4-x86_64.AppImage'
+fileinfo['LIN64_RC_URL']  = 'https://files.openscad.org/rc/OpenSCAD-2021.01-RC5-x86_64.AppImage'
 
 fileinfo['ARM64_RC_SIZE'] = '43 MB'
-fileinfo['ARM64_RC_URL']  = 'https://files.openscad.org/rc/OpenSCAD-2020.12-RC4-aarch64.AppImage'
+fileinfo['ARM64_RC_URL']  = 'https://files.openscad.org/rc/OpenSCAD-2021.01-RC5-aarch64.AppImage'
 
-fileinfo['SRC_RC_URL']    = 'https://files.openscad.org/rc/openscad-2020.12-RC4.src.tar.gz'
+fileinfo['SRC_RC_URL']    = 'https://files.openscad.org/rc/openscad-2021.01-RC5.src.tar.gz'
 fileinfo['SRC_RC_NAME']   = fileinfo['VERSION']
 fileinfo['SRC_RC_SIZE']   = '16 MB'
 
 fileinfo['MAC_RC_NAME']   = fileinfo['VERSION']
+fileinfo['MAC_RC_ASC_URL'] = fileinfo['MAC_RC_URL'] + '.asc'
 fileinfo['MAC_RC_SHA256_URL'] = fileinfo['MAC_RC_URL'] + '.sha256'
 fileinfo['MAC_RC_SHA512_URL'] = fileinfo['MAC_RC_URL'] + '.sha512'
 
@@ -29,28 +30,24 @@ fileinfo['WIN32_RC_INSTALLER_URL'] = fileinfo['WIN32_RC_BASE'] + '-Installer.exe
 fileinfo['WIN32_RC_INSTALLER_NAME'] = fileinfo['WIN_RC_NAME']
 fileinfo['WIN32_RC_INSTALLER_SIZE'] = fileinfo['WIN32_RC_SIZE']
 fileinfo['WIN32_RC_INSTALLER_ASC_URL'] = fileinfo['WIN32_RC_INSTALLER_URL'] + '.asc'
-fileinfo['WIN32_RC_INSTALLER_SHA1_URL'] = fileinfo['WIN32_RC_INSTALLER_URL'] + '.sha1'
 fileinfo['WIN32_RC_INSTALLER_SHA256_URL'] = fileinfo['WIN32_RC_INSTALLER_URL'] + '.sha256'
 fileinfo['WIN32_RC_INSTALLER_SHA512_URL'] = fileinfo['WIN32_RC_INSTALLER_URL'] + '.sha512'
 fileinfo['WIN32_RC_ZIP_URL'] = fileinfo['WIN32_RC_BASE'] + '.zip'
 fileinfo['WIN32_RC_ZIP_NAME'] = fileinfo['WIN_RC_NAME']
 fileinfo['WIN32_RC_ZIP_SIZE'] = fileinfo['WIN32_RC_SIZE']
 fileinfo['WIN32_RC_ZIP_ASC_URL'] = fileinfo['WIN32_RC_ZIP_URL'] + '.asc'
-fileinfo['WIN32_RC_ZIP_SHA1_URL'] = fileinfo['WIN32_RC_ZIP_URL'] + '.sha1'
 fileinfo['WIN32_RC_ZIP_SHA256_URL'] = fileinfo['WIN32_RC_ZIP_URL'] + '.sha256'
 fileinfo['WIN32_RC_ZIP_SHA512_URL'] = fileinfo['WIN32_RC_ZIP_URL'] + '.sha512'
 fileinfo['WIN64_RC_INSTALLER_URL'] = fileinfo['WIN64_RC_BASE'] + '-Installer.exe'
 fileinfo['WIN64_RC_INSTALLER_NAME'] = fileinfo['WIN_RC_NAME']
 fileinfo['WIN64_RC_INSTALLER_SIZE'] = fileinfo['WIN64_RC_SIZE']
 fileinfo['WIN64_RC_INSTALLER_ASC_URL'] = fileinfo['WIN64_RC_INSTALLER_URL'] + '.asc'
-fileinfo['WIN64_RC_INSTALLER_SHA1_URL'] = fileinfo['WIN64_RC_INSTALLER_URL'] + '.sha1'
 fileinfo['WIN64_RC_INSTALLER_SHA256_URL'] = fileinfo['WIN64_RC_INSTALLER_URL'] + '.sha256'
 fileinfo['WIN64_RC_INSTALLER_SHA512_URL'] = fileinfo['WIN64_RC_INSTALLER_URL'] + '.sha512'
 fileinfo['WIN64_RC_ZIP_URL'] = fileinfo['WIN64_RC_BASE'] + '.zip'
 fileinfo['WIN64_RC_ZIP_NAME'] = fileinfo['WIN_RC_NAME']
 fileinfo['WIN64_RC_ZIP_SIZE'] = fileinfo['WIN64_RC_SIZE']
 fileinfo['WIN64_RC_ZIP_ASC_URL'] = fileinfo['WIN64_RC_ZIP_URL'] + '.asc'
-fileinfo['WIN64_RC_ZIP_SHA1_URL'] = fileinfo['WIN64_RC_ZIP_URL'] + '.sha1'
 fileinfo['WIN64_RC_ZIP_SHA256_URL'] = fileinfo['WIN64_RC_ZIP_URL'] + '.sha256'
 fileinfo['WIN64_RC_ZIP_SHA512_URL'] = fileinfo['WIN64_RC_ZIP_URL'] + '.sha512'
 
