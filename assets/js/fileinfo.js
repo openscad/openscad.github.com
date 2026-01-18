dbed08a50c8dc9aee63924c0cbf744f09cad94ef
Joseph Shapiro <joeyashapiro@gmail.com>
2026-01-18 11:21:59 -0600

    match indenting of other files

--- assets/js/fileinfo.js
+++ assets/js/fileinfo.js
@@ -1,19 +1,19 @@
 var fileinfo = {};
 
 function setSnapshotFileInfo(key, name, size, url) {
-    fileinfo[key + "_NAME"] = name;
-    fileinfo[key + "_SIZE"] = size;
-    fileinfo[key + "_URL"] = url;
-    fileinfo[key + "_ASC_URL"] = url + ".asc";
-    fileinfo[key + "_SHA256_URL"] = url + ".sha256";
-    fileinfo[key + "_SHA512_URL"] = url + ".sha512";
+  fileinfo[key + "_NAME"] = name;
+  fileinfo[key + "_SIZE"] = size;
+  fileinfo[key + "_URL"] = url;
+  fileinfo[key + "_ASC_URL"] = url + ".asc";
+  fileinfo[key + "_SHA256_URL"] = url + ".sha256";
+  fileinfo[key + "_SHA512_URL"] = url + ".sha512";
 }
 jQuery(document).ready(function () {
-    for (var fileid in fileinfo) {
-        if (fileid.indexOf("_URL") >= 0) {
-            $("a[id=" + fileid + "]").attr('href', fileinfo[fileid]);
-        } else {
-            $("#" + fileid).text(fileinfo[fileid]);
-        }
+  for (var fileid in fileinfo) {
+    if (fileid.indexOf("_URL") >= 0) {
+      $("a[id=" + fileid + "]").attr('href', fileinfo[fileid]);
+    } else {
+      $("#" + fileid).text(fileinfo[fileid]);
     }
+  }
 });
